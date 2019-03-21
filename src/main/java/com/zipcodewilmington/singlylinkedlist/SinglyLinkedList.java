54effd6cf1f658c86d486e12a917d3d210ec7f00
Kate Bayard <katherinebayard@zipcoders-MBP-8.lan>
2019-03-20 22:45:57 -0400

    methods through size completed

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,97 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.List;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+
+    Node startingNode;
+
+    public SinglyLinkedList(Object element) {
+        this.startingNode = new Node(element);
+    }
+
+    public class Node {
+        Object element;
+        Node nextNode;
+
+        public Node(Object element) {
+            this.element = element;
+            this.nextNode = null;
+        }
+
+        public Object getElement() {
+            return element;
+        }
+
+        public void setElement(Object element) {
+            this.element = element;
+        }
+
+        public Node getNextNode() {
+            return nextNode;
+        }
+
+        public void updateNextNode(Node nextNode) {
+            this.nextNode = nextNode;
+        }
+    }
+
+    public void add(Object element) {
+        Node currentNode = startingNode;
+        while(currentNode.getNextNode() != null) {
+            currentNode = currentNode.getNextNode();
+        }
+        Node newNode = new Node(element);
+        currentNode.updateNextNode(newNode);
+    }
+
+    public void remove(int index) {
+        Node currentNode = startingNode;
+        int currentIndex = 0;
+        while (currentIndex < index) {
+            currentNode = currentNode.getNextNode();
+            currentIndex ++;
+        }
+        currentNode.setElement(null);
+    }
+
+    public boolean contains(Object elementToLookFor) {
+        if(find(elementToLookFor) != -1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int find(Object elementToLookFor) {
+        Node currentNode = startingNode;
+        int currentIndex = 0;
+        do {
+            if(currentNode.getElement() == elementToLookFor) {
+                return currentIndex;
+            } else {
+                currentNode = currentNode.getNextNode();
+                currentIndex++;
+            }
+        } while(currentNode != null);
+
+        return -1;
+    }
+
+    public int size() {
+        Node currentNode = startingNode;
+        int currentSize = 1;
+        do {currentNode = currentNode.getNextNode();
+            currentSize++;
+        } while(currentNode.getNextNode() != null);
+        return currentSize;
+    }
+
 }
+
+//get -- returns the element at the specified index
+//copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+//sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,162 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void listConstructorTest() {
+
+        //Given
+        Object element = "this element";
+
+        //When
+        SinglyLinkedList testList = new SinglyLinkedList(element);
+
+        //Then
+        Assert.assertTrue(testList instanceof SinglyLinkedList);
+        Assert.assertNotNull(testList.startingNode);
+    }
+
+    @Test
+    public void updateNextNodeTest() {
+        //Given
+        Object element1 = "this element";
+        SinglyLinkedList testList = new SinglyLinkedList(element1);
+        SinglyLinkedList.Node testNode = testList.startingNode;
+
+        //When
+        testList.startingNode.updateNextNode(testNode);
+
+        //Then
+        Assert.assertEquals(testList.startingNode, testList.startingNode.getNextNode());
+    }
+
+        @Test
+    public void addTest() {
+
+        //Given
+        Object element1 = "this element";
+        Object element2 = "new element";
+        SinglyLinkedList testList = new SinglyLinkedList(element1);
+
+        //When
+        testList.add(element2);
+
+        //Then
+        Assert.assertTrue(testList.startingNode.getNextNode() instanceof Object);
+    }
+
+    @Test
+    public void removeTest() {
+        //Given
+        Object element1 = "this element";
+        Object element2 = "new element";
+        SinglyLinkedList testList = new SinglyLinkedList(element1);
+        testList.add(element2);
+
+        //When
+        Assert.assertNotNull(testList.startingNode.getNextNode());
+        testList.remove(1);
+        SinglyLinkedList.Node secondIndex = testList.startingNode.getNextNode();
+
+        //Then
+        Assert.assertNull(secondIndex.getElement());
+    }
+
+    @Test
+    public void containsTestReturnsTrue() {
+        //Given
+        Object element0 = "this element";
+        Object element1 = "new element";
+        Object element2 = "another element";
+        SinglyLinkedList testList = new SinglyLinkedList(element0);
+        testList.add(element1);
+        testList.add(element2);
+
+        //When
+        boolean result = testList.contains(element2);
+
+        //Then
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void containsTestReturnsFalse() {
+        //Given
+        Object element0 = "this element";
+        Object element1 = "new element";
+        Object element2 = "another element";
+        SinglyLinkedList testList = new SinglyLinkedList(element0);
+        testList.add(element1);
+
+        //When
+        boolean result = testList.contains(element2);
+
+        //Then
+        Assert.assertFalse(result);
+    }
+
+
+    @Test
+    public void findTestReturnsPositive() {
+        //Given
+        Object element0 = "this element";
+        Object element1 = "new element";
+        Object element2 = "another element";
+        SinglyLinkedList testList = new SinglyLinkedList(element0);
+        testList.add(element1);
+        testList.add(element2);
+
+        //When
+        int result = testList.find(element2);
+
+        //Then
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void findTestReturnsNegative() {
+        //Given
+        Object element0 = "this element";
+        Object element1 = "new element";
+        Object element2 = "another element";
+        SinglyLinkedList testList = new SinglyLinkedList(element0);
+        testList.add(element1);
+
+        //When
+        int result = testList.find(element2);
+
+        //Then
+        Assert.assertEquals(-1, result);
+    }
+
+    @Test
+    public void sizeTest() {
+        //Given
+        Object element0 = "this element";
+        Object element1 = "new element";
+        Object element2 = "another element";
+        SinglyLinkedList testList = new SinglyLinkedList(element0);
+        testList.add(element1);
+        testList.add(element2);
+
+        //When
+        int result = testList.size();
+
+        //Then
+        Assert.assertEquals(3, result);
+    }
+
+    //    @Test
+//    public void nodeConstructorTest() {
+//
+//
+//
+//    }
 }
